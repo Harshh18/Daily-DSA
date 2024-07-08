2f2cc09d5ee93fad4188823f7eec8b3d654b6f62
Harsh Khandelwal <harskhandelwal1805@gmail.com>
2024-07-08 17:11:49 +0530

    [1929] [Time Beats: 96.04%] [Memory Beats: 51.14%] - LeetPush

--- 1929-Concatenation-of-Array.java
+++ 1929-Concatenation-of-Array.java
@@ -1,12 +1,9 @@
 class Solution {
     public int[] getConcatenation(int[] nums) {
         int[] ans = new int[2*nums.length];
-        for(int i=0; i<ans.length; i++){
-            if(i>=nums.length){
-                ans[i]=nums[i-nums.length];
-            } else{
-                ans[i]=nums[i];
-            }
+        for(int i=0; i<nums.length; i++){
+           ans[i]=nums[i];
+           ans[i+nums.length]=nums[i];
         }
         return ans;
     }
